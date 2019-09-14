42d22306085430ee9d950b2a3d296c1b4866a7e1
Lai Zit Seng <lzs@zitseng.com>
2019-09-14 09:03:11 +0800

    Initial commit

---
+++ qrcallback.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use CGI;
+use JSON::PP;
+use HTML::Entities;
+
+    my $q = CGI->new;
+
+    print $q->header("application/json");
+    my $s = $q->param('s');
+    my $c = $q->param('c');
+
+    my ($nusnet, $time, $sig) = split(/;/, $s);
+
+    my $ltime = localtime($time);
+
+    my $s_ = encode_entities($s);
+    my $html = '';
+
+    if (!$c) {
+        $html = <<EOM;
+<p>Read from QR-code: $s_</p>
+<button class="ui-btn ui-corner-all" id="cb-confirm">CONFIRM</button>
+<button class="ui-btn ui-corner-all" id="cb-clear">Clear</button>
+<input type="hidden" name="cb-content" id="cb-content" value="$s_" />
+EOM
+    }
+    else {
+        $html = <<EOM;
+<p>CONFIRMED: $s_</p>
+EOM
+        status => 0
+    }
+
+    my $r = {
+        html => $html,
+        status => 0
+    };
+
+    $json = JSON::PP->new->ascii->pretty->allow_nonref;
+
+    print $json->encode($r);
